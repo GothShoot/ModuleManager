dfdb84ad41f95015e8921b5a607dc95df5e30ff8
GothShoot <romuald.mocq@yahoo.com>
2019-01-29 00:24:26 +0100

    Initial commit

---
+++ Services/ModuleManager.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alzundaz\ModuleManager\Services;
+
+use Alzundaz\NitroPHP\Services\ConfigHandler;
+use Alzundaz\NitroPHP\BaseClass\Singleton;
+
+
+class ModuleManager extends Singleton
+{
+    public function loadModule()
+    {
+
+    }
+
+    public function addModule($enabled = false)
+    {
+
+    }
+
+    /**
+     * Update module.json module list with installed modules
+     *
+     * @return void
+     */
+    private function listInstalledModule():void
+    {
+        $configHandler = ConfigHandler::getInstance();
+        $modules = $configHandler->loadJsonConfig(ROOT_DIR.'/Config/', 'module.json');
+        $raw_files = scandir( ROOT_DIR.'/Module/' );
+        foreach($raw_files as $file){
+            if ($file != '.' && $file != '..') {
+                $data = $configHandler->loadJsonConfig(ROOT_DIR.'/Module/'.$file.'/Config/');
+                if ( !array_key_exists($file, $modules) ){
+                    $data['enabled'] = false;
+                    $data['path'] = 'Module/'.$file;
+                }
+                array_replace($modules[$file], $data);
+            }
+        }
+        $configHandler->setConfig(ROOT_DIR.'/Config/module.json', $modules);
+    }
+}
